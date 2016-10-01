52864157c50c47a4f80f96b989cf7bbbe0b0bac3
Nicholas Vargas <mail@nickvargas.net>
2016-09-30 17:26:32 -0700

    Enable customizable node red domain

--- src/views/fileupload.js
+++ src/views/fileupload.js
@@ -161,7 +161,8 @@ exports.initFileUpload = function(ctx) {
     fileUploadDialog.val(null);
 
     var question = encodeURIComponent($('#resultsText').val())
-    var url = "https://watsondemos.mybluemix.net/HelloWorld?q="+question;
+    var host = $('#noderedurl').val();
+    var url = "https://" + host + "/text?q="+question;
 
     var audio2 = window.document.createElement('audio');
     audio2.style.display = "none";
